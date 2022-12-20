a16af58c36ef652159957c1f903153e293443202
Mario <mariogr901@gmail.com>
2022-12-20 21:39:23 +0100

    Enunciado agregado.

--- src/main/kotlin/server/Servidor.kt
+++ src/main/kotlin/server/Servidor.kt
@@ -8,6 +8,17 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 
+/*
+-- ENUNCIADO (segun recuerdo) --
+R2D2 crea una seccion critica llamada StarUnix, y se pueden enviar tres tipos de mensajes:
+    - Por parte de un piloto, envia un registro de una nave
+    - Por parte de BB8, busca el numero de misiles totales y el total de naves
+    - Por parte de Luke, el quiere el listado de registros/naves al completo.
+Nave: Id, tipo de Nave(X-WIND, T-FIGHTER), salto de hiper espacio (boolean), misiles protonicos (entre 10..20), fecha creacion
+Tiempo para resolverlo: 2h:30min
+
+* */
+
 private val log = KotlinLogging.logger {}
 
 private const val PUERTO = 6969
